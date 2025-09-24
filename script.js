891979b9e45f59293069639e9487877e70236e15
Ishika Verma <ishikaverma2317@gmail.com>
2025-09-24 10:22:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,15 +1,4 @@
 function convertToRoman(num) {
-  	const obj = {
-      0:['M',1000], 
-      1:['D', 500], 
-      2:['C', 100], 
-      3:['L', 50], 
-      4:['X', 10], 
-      5:['V', 5], 
-      6:['I', 1]
-    };
-
-  function convertToRoman(num) {
     if (num === 0) return ""; // Romans did not have zero
 
     const romanMap = [
@@ -39,15 +28,3 @@ function convertToRoman(num) {
 
     return result;
 }
-
-
-}
-// You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
-
-// console.log(convertToRoman(36));
-
-
-
-
-// do not edit below this line
-module.exports = convertToRoman
