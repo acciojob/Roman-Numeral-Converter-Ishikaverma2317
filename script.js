229dbe3d14965e18c38f515d5f506c8e5b160bce
Ishika Verma <ishikaverma2317@gmail.com>
2025-09-24 10:26:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,37 +1,34 @@
-function convertToRoman(num) {
-    if (num === 0) return ""; // Romans did not represent zero
+function convertToRoman() {
+  let num = parseInt(document.getElementById("numberInput").value);
 
-    const romanMap = [
-        ["M", 1000],
-        ["CM", 900],
-        ["D", 500],
-        ["CD", 400],
-        ["C", 100],
-        ["XC", 90],
-        ["L", 50],
-        ["XL", 40],
-        ["X", 10],
-        ["IX", 9],
-        ["V", 5],
-        ["IV", 4],
-        ["I", 1]
-    ];
+  if (isNaN(num) || num < 0 || num > 100000) {
+    document.getElementById("result").innerText = "Enter a number between 0 and 100000";
+    return;
+  }
 
-    let result = "";
+  const romanMap = [
+    ["M", 1000],
+    ["CM", 900],
+    ["D", 500],
+    ["CD", 400],
+    ["C", 100],
+    ["XC", 90],
+    ["L", 50],
+    ["XL", 40],
+    ["X", 10],
+    ["IX", 9],
+    ["V", 5],
+    ["IV", 4],
+    ["I", 1]
+  ];
 
-    for (let [symbol, value] of romanMap) {
-        while (num >= value) {
-            result += symbol;
-            num -= value;
-        }
+  let result = "";
+  for (let [symbol, value] of romanMap) {
+    while (num >= value) {
+      result += symbol;
+      num -= value;
     }
+  }
 
-    return result;
+  document.getElementById("result").innerText = result;
 }
-
-// --- Do NOT change anything below ---
-// Input handling for judge
-const fs = require("fs");
-const input = fs.readFileSync(0, "utf-8").trim();
-const n = parseInt(input, 10);
-console.log(convertToRoman(n));
